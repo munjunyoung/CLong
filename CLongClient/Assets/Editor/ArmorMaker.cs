fa516988ffd147cb01a7ca806449fe85789d1c8a
Rekdoll <minsterr38@gmail.com>
2018-07-13 02:31:48 +0900

    총/방어구 생성 에디터 스크립트 추가

---
+++ CLongClient/Assets/Editor/ArmorMaker.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ArmorMaker : EditorWindow
+{
+    private enum ARMOR_TYPE
+    {
+        HELMET,
+        VEST
+    }
+
+    private ARMOR_TYPE _type;
+    private string _name = "UNKNOWN";
+    private float _reduceValue = 0;
+
+    private static ArmorMaker window;
+
+    [MenuItem("Custom Tool/Item Maker/Armor")]
+    static void Init()
+    {
+        window = (ArmorMaker)EditorWindow.GetWindow<ArmorMaker>();
+        window.Show();
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Base Properties", EditorStyles.boldLabel);
+
+        _name = EditorGUILayout.TextField("Name", _name);
+        _type = (ARMOR_TYPE)EditorGUILayout.EnumPopup("Type", _type);
+        _reduceValue = EditorGUILayout.FloatField("Damage Reduction Rate", _reduceValue);
+
+        GUILayout.Label("Detail Properties", EditorStyles.boldLabel);
+
+        switch (_type)
+        {
+            case ARMOR_TYPE.HELMET:
+                break;
+            case ARMOR_TYPE.VEST:
+                break;
+        }
+
+        if (GUILayout.Button("Confirm"))
+        {
+            // Make Json data
+        }
+        else if(GUILayout.Button("Cancel"))
+        {
+            // Cancel
+            window.Close();
+        }
+    }
+}
---
+++ CLongClient/Assets/Editor/WeaponMaker.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class WeaponMaker : EditorWindow
+{
+    private enum WEAPON_TYPE
+    {
+        HG, SMG, AR, SR
+    }
+
+    // 유형
+    private WEAPON_TYPE _type;
+    // 이름
+    private string _name = "UNKNOWN";
+    // 장탄수
+    private int _bulletNum = 10;
+    // 탄창수 
+    private int _maxMagazine = 3;
+    // 총알 속도
+    private float _bulletSpeed = 1f;
+    // 연사 속도
+    private float _fireSpeed = 1f;
+    // 무게
+    private float _weight = 1f;
+    // 재장전 속도
+    private float _reloadTime = 1f;
+    // 반동
+    private float _rebound = 1f;
+
+    private static WeaponMaker window;
+
+    [MenuItem("Custom Tool/Item Maker/Weapon")]
+    static void Init()
+    {
+        window = (WeaponMaker)EditorWindow.GetWindow<WeaponMaker>();
+        window.Show();
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Properties", EditorStyles.boldLabel);
+
+        _name = EditorGUILayout.TextField("Name", _name);
+        _type = (WEAPON_TYPE)EditorGUILayout.EnumPopup("Weapon Type", _type);
+        _bulletNum = EditorGUILayout.IntField("Number of Bullets", _bulletNum);
+        _maxMagazine = EditorGUILayout.IntField("Maximum Magazine", _maxMagazine);
+        _bulletSpeed = EditorGUILayout.Slider("Bullet Speed Factor", _bulletSpeed, 1, 5);
+        _fireSpeed = EditorGUILayout.Slider("Fire Speed Factor", _fireSpeed, 1, 5);
+        _weight = EditorGUILayout.FloatField("Weight", _weight);
+        _reloadTime = EditorGUILayout.Slider("Reload Time", _reloadTime, 1, 5);
+        _rebound = EditorGUILayout.FloatField("Rebound Value", _rebound);
+
+        if(GUILayout.Button("Confirm"))
+        {
+            // Make Json data
+        }
+        else if (GUILayout.Button("Cancel"))
+        {
+            // Cancel
+            window.Close();
+        }
+    }
+}
