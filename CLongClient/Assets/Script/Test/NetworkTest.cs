f3aeb9cdc8b5331060b0bdc1ee173229f3949cd2
Rekdoll <minsterr38@gmail.com>
2018-07-22 02:06:27 +0900

    패킷구조 짜는중

---
+++ CLongClient/Assets/Script/Test/NetworkTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Net;
+using UnityEngine;
+using CLongLib;
+
+public class NetworkTest : MonoBehaviour
+{
+    UdpClient _c;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _c = new UdpClient();
+
+        var d = new Login_Req
+        {
+            id = "1123",
+            password = "pasd"
+        };
+        IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 30000);
+        var pb = PacketMaker.MakePacket(PacketMaker.CODE_MOVE, d);
+        Debug.Log(_c.Send(pb, pb.Length, ep));
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		
+	}
+}
---
+++ CLongLib/CLongLib/PacketStructure.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace CLongLib
+{
+    public class PacketMaker
+    {
+        public static byte CODE_MOVE { get { return 0x00; } }
+        
+        public static byte[] MakePacket(byte CODE, object o)
+        {
+            Type t = o.GetType();
+            int size = Marshal.SizeOf(o);
+            IntPtr p = Marshal.AllocHGlobal(size);
+
+            byte[] ary = new byte[size];
+            Marshal.Copy(p, ary, 0, size);
+
+            return ary;
+        }
+    }
+
+    /// <summary>
+    /// Login request from CLIENT to SERVER.
+    /// </summary>
+    public struct Login_Req
+    {
+        public string id;
+        public string password;
+    }
+
+    /// <summary>
+    /// Login ackknowledgement from SERVER to CLIENT
+    /// </summary>
+    public struct Login_Ack
+    {
+        public bool connected;
+    }
+
+    /// <summary>
+    /// Matching start/cancel request from CLIENT to SERVER.
+    /// </summary>
+    public struct Match_Req
+    {
+        public bool matching;
+    }
+
+    public struct Match_Ack
+    {
+        public bool matching;
+    }
+}
