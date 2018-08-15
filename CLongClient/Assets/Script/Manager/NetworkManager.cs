f5d44df2fbb1867754ab9bad50adf1b585873528
Rekdoll <minsterr38@gmail.com>
2018-08-16 02:38:13 +0900

    송수신 코드 수정중

--- CLongClient/Assets/Script/Manager/NetworkManager.cs
+++ CLongClient/Assets/Script/Manager/NetworkManager.cs
@@ -1,14 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
 using UnityEngine;
 using CLongLib;
 
 public class NetworkManager : MonoBehaviour
 {
     public enum Protocol { TCP, UDP }
-
+    
     private const string _IP = "127.0.0.1";
     private const int _PORT = 23000;
+
+    private const float _TCP_PROC_FREQ = 0.01f;
+    private const float _UDP_PROC_FREQ = 0.01f;
 
     private TCPNetwork _tcpNet = new TCPNetwork();
     private UDPNetwork _udpNet = new UDPNetwork();
@@ -29,10 +35,34 @@ public class NetworkManager : MonoBehaviour
     private void Awake()
     {
         _tcpNet.Init(_IP, _PORT);
+        StartCoroutine(ProcessPacket());
+    }
+    float elapsedTime = 0;
+    private IEnumerator ProcessPacket()
+    {
+        int c = 0;
+        
+        while(true)
+        {
+            c++;
+            if(_tcpNet.packetQueue.Count > 0)
+            {
+                ProcessPacket(_tcpNet.packetQueue.Dequeue());
+            }
+            if (c % 100 == 0)
+            {
+                Debug.Log("100 processing time : " + DateTime.Now + " " + DateTime.Now.Millisecond + ", elapsedTime : " + elapsedTime);
+                c = 0;
+                elapsedTime = 0;
+            }
+
+            yield return new WaitForFixedUpdate();
+        }
     }
 
     private void FixedUpdate()
     {
+        elapsedTime += Time.deltaTime;
         if (Input.GetKeyDown(KeyCode.F4))
         {
             this.SendPacket(new Queue_Req { req = true }, Protocol.TCP);
@@ -40,6 +70,9 @@ public class NetworkManager : MonoBehaviour
 
         if (_tcpNet.packetQueue.Count > 0)
             ProcessPacket(_tcpNet.packetQueue.Dequeue());
+
+        if (_udpNet.packetQueue.Count > 0)
+            ProcessPacket(_udpNet.packetQueue.Dequeue());
     }
 
     private void ProcessPacket(IPacket p)
@@ -48,6 +81,118 @@ public class NetworkManager : MonoBehaviour
         {
             var s = (Start_Game)p;
             _udpNet.Init(s.ip, s.port);
+        }
+    }
+
+    private class TCPNetwork
+    {
+        internal Queue<IPacket> packetQueue = new Queue<IPacket>();
+        private TcpClient tcp;
+
+        private byte[] _recvBuffer = new byte[4096];
+
+        public void Init(string ip, int port)
+        {
+            tcp = new TcpClient();
+            tcp.BeginConnect(ip, port, ConnectCb, tcp);
+        }
+
+        public void Send(IPacket p)
+        {
+            try
+            {
+                var b = PacketMaker.SetPacket(p);
+                tcp.Client.BeginSend(b, 0, b.Length, SocketFlags.None, null, tcp.Client);
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e);
+            }
+        }
+
+        private void ConnectCb(IAsyncResult ar)
+        {
+            var tc = (TcpClient)ar.AsyncState;
+            tc.Client.BeginReceive(_recvBuffer, 0, _recvBuffer.Length, SocketFlags.None,
+                ReceiveCb, tc.Client);
+        }
+
+        private void ReceiveCb(IAsyncResult ar)
+        {
+            try
+            {
+                var sock = (Socket)ar.AsyncState;
+                int recvSize = sock.EndReceive(ar);
+
+                if (recvSize > 0)
+                {
+                    Debug.Log("[TCP] Received data size : " + recvSize);
+                    var dataAry = new byte[recvSize];
+                    Array.Copy(_recvBuffer, 0, dataAry, 0, recvSize);
+                    PacketMaker.GetPacket(dataAry, ref packetQueue);
+                    Array.Clear(_recvBuffer, 0, _recvBuffer.Length);
+                    Debug.Log("[TCP] Process was done.");
+                }
+                else
+                {
+                    Debug.Log("[TCP] Receive no data.");
+                }
+                sock.BeginReceive(_recvBuffer, 0, _recvBuffer.Length, SocketFlags.None,
+                        ReceiveCb, sock);
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e);
+            }
+        }
+    }
+
+    public class UDPNetwork
+    {
+        internal Queue<IPacket> packetQueue = new Queue<IPacket>();
+        private UdpClient udp;
+
+        public void Init(uint ip, int port)
+        {
+            udp = new UdpClient();
+            udp.Client.Bind(new IPEndPoint(IPAddress.Any, 23010));
+            var addr = new byte[] { (byte)(ip >> 24), (byte)(ip >> 16),
+            (byte)(ip >> 8), (byte)ip };
+            udp.JoinMulticastGroup(new IPAddress(addr));
+            udp.BeginReceive(ReceiveCb, udp);
+        }
+
+        public void Send(IPacket p)
+        {
+            try
+            {
+                var b = PacketMaker.SetPacket(p);
+                udp.BeginSend(b, b.Length, null, udp);
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e);
+            }
+        }
+
+        private void ReceiveCb(IAsyncResult ar)
+        {
+            try
+            {
+                var uc = (UdpClient)ar.AsyncState;
+                var remoteEp = new IPEndPoint(IPAddress.Any, 0);
+                var dataAry = uc.EndReceive(ar, ref remoteEp);
+                if (dataAry.Length > 0)
+                {
+                    Debug.Log("[UDP] Received data size : " + dataAry.Length);
+                    PacketMaker.GetPacket(dataAry, ref packetQueue);
+                }
+                uc.BeginReceive(ReceiveCb, uc);
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e);
+            }
         }
     }
 }
--- CLongLib/CLongLib/PacketMaker.cs
+++ CLongLib/CLongLib/PacketMaker.cs
@@ -11,24 +11,30 @@ namespace CLongLib
 {
     public class PacketMaker
     {
+        private const int _HEADER_SIZE = 1;
+        private enum PType
+        {
+            a = 0, b, c
+        }
+
         private static readonly Dictionary<Type, byte> _typeDic = new Dictionary<Type, byte>()
         {
             {typeof(Login_Ack), 0x00},
             {typeof(Login_Req), 0x01},
-            {typeof(Start_Game), 0x02 },
+            {typeof(Start_Game), 0x02},
             {typeof(Queue_Req), 0x03 }
         };
 
         public static byte[] SetPacket(object o)
         {
             int size = Marshal.SizeOf(o);
-            byte[] ary = new byte[size+1];
+            byte[] ary = new byte[size+ _HEADER_SIZE];
             IntPtr p = Marshal.AllocHGlobal(size);
 
             ary[0] = _typeDic[o.GetType()];
 
             Marshal.StructureToPtr(o, p, true);
-            Marshal.Copy(p, ary, 1, size);
+            Marshal.Copy(p, ary, _HEADER_SIZE, size);
             Marshal.FreeHGlobal(p);
 
             return ary;
@@ -43,14 +49,14 @@ namespace CLongLib
                 var type = _typeDic.FirstOrDefault(x => x.Value == b[headerIdx]).Key;
                 int size = Marshal.SizeOf(type);
 
-                if (headerIdx + size +1 > b.Length)
+                if (headerIdx + size + _HEADER_SIZE > b.Length)
                     throw new Exception();
 
                 IntPtr ptr = Marshal.AllocHGlobal(size);
-                Marshal.Copy(b, 1, ptr, size);
+                Marshal.Copy(b, _HEADER_SIZE, ptr, size);
                 IPacket packet = Marshal.PtrToStructure(ptr, type) as IPacket;
                 Marshal.FreeHGlobal(ptr);
-                headerIdx = headerIdx + size + 1;
+                headerIdx = headerIdx + size + _HEADER_SIZE;
 
                 list.Enqueue(packet);
             }
